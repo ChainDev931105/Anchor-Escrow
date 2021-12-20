b9c0812fca13ff29d1607580911062e034126338
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2021-12-20 13:35:41 -0400

    add header of unittest

--- tests/anchor-escrow.ts
+++ tests/anchor-escrow.ts
@@ -1,5 +1,7 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import { PublicKey, Keypair, SystemProgram } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+import { assert } from "chai";
 import { AnchorEscrow } from '../target/types/anchor_escrow';
 
 describe('anchor-escrow', () => {
@@ -14,4 +16,24 @@ describe('anchor-escrow', () => {
     const tx = await program.rpc.initialize({});
     console.log("Your transaction signature", tx);
   });
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  
+
+  let mintA: Token = null;
+  let mintB: Token = null;
+  let initializerTokenAccountA: PublicKey = null;
+  let initializerTokenAccountB: PublicKey = null;
+  let takerTokenAccountA: PublicKey = null;
+  let takerTokenAccountB: PublicKey = null;
+  let pda: PublicKey = null;
+
+  const takerAmount = 1000;
+  const initializerAmount = 500;
+
+  const escrowAccount = Keypair.generate();
+  const payer = Keypair.generate();
+  const mintAuthority = Keypair.generate();
+  
 });
